721d0435a655b7043408e85ba610b1ead8c1c8e5
Roy Hershkovitz <roy.hershkovitz@mce-sys.com>
2015-08-05 12:38:37 +0300

    finish

--- app/routes.js
+++ app/routes.js
@@ -38,7 +38,12 @@ module.exports = function(app, passport) {
         failureFlash : true // allow flash messages
     }));
 
-
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
     // =====================================
     // PROFILE SECTION =====================
     // =====================================
